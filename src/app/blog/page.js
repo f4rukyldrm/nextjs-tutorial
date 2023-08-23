240fb198a20424fee694e403158378f138355cba
f4rukyldrm <faruk-yildirim@outlook.com.tr>
2023-08-23 17:59:45 +0300

    ...

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 async function getData() {
     try {
-        const res = await fetch("http://127.0.0.1:3000/api/posts", {
+        const res = await fetch("http://localhost:3000/api/posts", {
             cache: "no-store",
         });
 
